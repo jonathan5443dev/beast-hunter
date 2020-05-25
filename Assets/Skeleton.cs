bf75fab64eaa767fd1c6e873f8554c1732230215
Jonathan Buitrago <jonathan.buitrago@grability.com>
2020-05-24 22:50:13 -0500

    week 1 and 2

---
+++ Assets/Skeleton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour
+{
+    public float speed = -1f;
+    Rigidbody2D rigidBody;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D> ();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocityVector = new Vector2 (speed, 0);   
+        rigidBody.velocity = velocityVector;  
+    }
+}
