e65c853bdbff8ecf1da07a07d392e25c7767c89b
Jonathan Buitrago <jonathan.buitrago@grability.com>
2020-05-26 23:36:12 -0500

    add skeleton limits

--- Assets/Skeleton.cs
+++ Assets/Skeleton.cs
@@ -6,14 +6,32 @@ public class Skeleton : MonoBehaviour
 {
     public float speed = -1f;
     Rigidbody2D rigidBody;
+    
+    // Constructor
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D> ();
     }
 
+    // Run every frame
     void FixedUpdate()
     {
         Vector2 velocityVector = new Vector2 (speed, 0);   
         rigidBody.velocity = velocityVector;  
     }
+
+    // Trigger limits collision
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Flip();
+    }
+    
+    // Change x direction
+    void Flip() {
+        speed *= -1;
+        var scale =  transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
 }
