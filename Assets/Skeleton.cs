bcd196837e92560e76db3a5180cc31aa5ddca8dd
Jonathan Buitrago <jonathan.buitrago@grability.com>
2020-05-28 00:04:54 -0500

    fix code format

--- Assets/Skeleton.cs
+++ Assets/Skeleton.cs
@@ -8,48 +8,48 @@ public class Skeleton : MonoBehaviour {
     Animator anim;
 
     // Constructor
-    void Start() {
+    void Start () {
         rigidBody = GetComponent<Rigidbody2D> ();
         anim = GetComponent<Animator> ();
     }
 
     // Run every frame
-    void FixedUpdate() {
-        setAnimationState();
+    void FixedUpdate () {
+        setAnimationState ();
     }
 
     // Trigger limits collision
-    void OnTriggerEnter2D(Collider2D col) {
-        Flip();
+    void OnTriggerEnter2D (Collider2D col) {
+        Flip ();
     }
-    
+
     // Change x direction
-    void Flip() {
+    void Flip () {
         speed *= -1;
-        var scale =  transform.localScale;
+        var scale = transform.localScale;
         scale.x *= -1;
         transform.localScale = scale;
     }
 
     // Change animations states
-    void setAnimationState() {      
-        bool isWalking = anim.GetCurrentAnimatorStateInfo(0).IsName("skeleton_walking");
-        bool shouldIdle = Random.value < 1f/(60f*3f);
-        if(isWalking){
-            if(shouldIdle){
-                anim.SetTrigger("idle");
-            }else{
-                Vector2 velocityVector = new Vector2 (speed, 0);   
+    void setAnimationState () {
+        bool isWalking = anim.GetCurrentAnimatorStateInfo (0).IsName ("skeleton_walking");
+        bool shouldIdle = Random.value < 1f / (60f * 3f);
+        if (isWalking) {
+            if (shouldIdle) {
+                anim.SetTrigger ("idle");
+            } else {
+                Vector2 velocityVector = new Vector2 (speed, 0);
                 rigidBody.velocity = velocityVector;
             }
-        }else{
-            bool shouldWalk= Random.value < 1f/(60f*3f);
-            bool shouldAttack= Random.value < 1f/(60f*3f);
-            if(shouldWalk){
-                anim.SetTrigger("walk");
+        } else {
+            bool shouldWalk = Random.value < 1f / (60f * 3f);
+            bool shouldAttack = Random.value < 1f / (60f * 3f);
+            if (shouldWalk) {
+                anim.SetTrigger ("walk");
             }
-            if(shouldAttack){
-                anim.SetTrigger("attack");
+            if (shouldAttack) {
+                anim.SetTrigger ("attack");
             }
         }
     }
