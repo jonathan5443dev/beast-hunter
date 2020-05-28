0edb185067007816e765e8bc2fae0caa50cfa406
Jonathan Buitrago <jonathan.buitrago@grability.com>
2020-05-28 00:03:36 -0500

    switch animation state

--- Assets/Skeleton.cs
+++ Assets/Skeleton.cs
@@ -2,27 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skeleton : MonoBehaviour
-{
+public class Skeleton : MonoBehaviour {
     public float speed = -1f;
     Rigidbody2D rigidBody;
-    
+    Animator anim;
+
     // Constructor
-    void Start()
-    {
+    void Start() {
         rigidBody = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator> ();
     }
 
     // Run every frame
-    void FixedUpdate()
-    {
-        Vector2 velocityVector = new Vector2 (speed, 0);   
-        rigidBody.velocity = velocityVector;  
+    void FixedUpdate() {
+        setAnimationState();
     }
 
     // Trigger limits collision
-    void OnTriggerEnter2D(Collider2D col)
-    {
+    void OnTriggerEnter2D(Collider2D col) {
         Flip();
     }
     
@@ -32,6 +29,29 @@ public class Skeleton : MonoBehaviour
         var scale =  transform.localScale;
         scale.x *= -1;
         transform.localScale = scale;
+    }
+
+    // Change animations states
+    void setAnimationState() {      
+        bool isWalking = anim.GetCurrentAnimatorStateInfo(0).IsName("skeleton_walking");
+        bool shouldIdle = Random.value < 1f/(60f*3f);
+        if(isWalking){
+            if(shouldIdle){
+                anim.SetTrigger("idle");
+            }else{
+                Vector2 velocityVector = new Vector2 (speed, 0);   
+                rigidBody.velocity = velocityVector;
+            }
+        }else{
+            bool shouldWalk= Random.value < 1f/(60f*3f);
+            bool shouldAttack= Random.value < 1f/(60f*3f);
+            if(shouldWalk){
+                anim.SetTrigger("walk");
+            }
+            if(shouldAttack){
+                anim.SetTrigger("attack");
+            }
+        }
     }
 
 }
