782c361465421407f1b6245e56f306acaf82520c
Jonathan Buitrago <jonathan.buitrago@grability.com>
2020-06-22 11:50:14 -0500

    fix death an animations

---
+++ Assets/EnemyTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrigger : MonoBehaviour {
+    // Start is called before the first frame update
+    Collider2D colliderEnem = null;
+    public int delay = 1;
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    private void OnTriggerEnter2D (Collider2D other) {
+        if (other.gameObject.name.Equals ("skeleton") && colliderEnem == null) {
+            colliderEnem = other;
+            Invoke ("ActivateAttackMode", delay);
+        }
+    }
+
+    private void OnTriggerExit2D (Collider2D other) {
+        if (other == colliderEnem) {
+            Invoke ("DisableAttackMode", delay);
+        }
+    }
+
+    void ActivateAttackMode () {
+        Debug.Log ("prender ataque");
+        colliderEnem.gameObject.GetComponent<Skeleton> ().AttackMode (true);
+    }
+
+    void DisableAttackMode () {
+        Debug.Log ("apagar ataque");
+        colliderEnem.gameObject.GetComponent<Skeleton> ().AttackMode (false);
+    }
+
+}
--- Assets/Skeleton.cs
+++ Assets/Skeleton.cs
@@ -10,21 +10,22 @@ public class Skeleton : MonoBehaviour {
     public float speed = -1f;
     public Slider slider;
     public Text txt;
+    public Text defeatedTxt;
 
     public float energy = 100;
+    bool attackingMode = false;
+    bool isDeath = false;
 
     void Start () {
         rigidBody = GetComponent<Rigidbody2D> ();
         anim = GetComponent<Animator> ();
         rigidBody.freezeRotation = true;
+
     }
 
     void Update () {
         if (!anim.GetCurrentAnimatorStateInfo (0).IsName ("skeleton_death")) {
-            if (energy <= 0) {
-                energy = 0;
-                anim.SetTrigger ("death");
-            }
+            CheckDeath ();
         } else {
             return;
         }
@@ -52,6 +53,11 @@ public class Skeleton : MonoBehaviour {
         energy -= 10;
     }
 
+    // AttackMode
+    public void AttackMode (bool selection) {
+        attackingMode = selection;
+    }
+
     // Change x direction
     void Flip () {
         speed *= -1;
@@ -60,27 +66,49 @@ public class Skeleton : MonoBehaviour {
         transform.localScale = scale;
     }
 
-    // Change animation states
-    void SetAnimationState () {
-        bool isWalking = anim.GetCurrentAnimatorStateInfo (0).IsName ("skeleton_walking");
-        bool shouldIdle = Random.value < 1f / (60f * 3f);
-        if (isWalking) {
-            if (shouldIdle) {
-                anim.SetTrigger ("idle");
-            } else {
-                Vector2 velocityVector = new Vector2 (speed, 0);
-                rigidBody.velocity = velocityVector;
-            }
-        } else {
-            bool shouldWalk = Random.value < 1f / (60f * 3f);
-            bool shouldAttack = Random.value < 1f / (60f * 3f);
-            if (shouldWalk) {
-                anim.SetTrigger ("walk");
-            }
-            if (shouldAttack) {
-                anim.SetTrigger ("attack");
-            }
+    // Death
+    void CheckDeath () {
+        if (energy <= 0) {
+            energy = 0;
+            anim.SetTrigger ("death");
+            isDeath = true;
+            Destroy (gameObject, 1.5f);
+            defeatedTxt.text = "Eliminado!!!";
         }
     }
 
+    // Change animation states
+    void SetAnimationState () {
+        if (!isDeath) {
+            if (attackingMode) {
+                var scale = transform.localScale;
+                if (scale.x < 1) {
+                    attackingMode = true;
+                    scale.x *= -1;
+                    transform.localScale = scale;
+                }
+                anim.SetTrigger ("attack");
+            } else {
+                bool isWalking = anim.GetCurrentAnimatorStateInfo (0).IsName ("skeleton_walking");
+                bool shouldIdle = Random.value < 1f / (60f * 3f);
+                if (isWalking) {
+                    if (shouldIdle) {
+                        anim.SetTrigger ("idle");
+                    } else {
+                        Vector2 velocityVector = new Vector2 (speed, 0);
+                        rigidBody.velocity = velocityVector;
+                    }
+                } else {
+                    bool shouldWalk = Random.value < 1f / (60f * 3f);
+                    bool shouldAttack = Random.value < 1f / (60f * 3f);
+                    if (shouldWalk) {
+                        anim.SetTrigger ("walk");
+                    }
+                    if (shouldAttack) {
+                        anim.SetTrigger ("attack");
+                    }
+                }
+            }
+        }
+    }
 }
